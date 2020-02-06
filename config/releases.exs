9382fc0fd4b86935c4a9c9eca0c4c5303fb71c14
Mickey Chen <hey.mickabo@gmail.com>
2020-02-06 15:16:58 +0800

    migrate to the new config, added release.exs

--- config/releases.exs
+++ config/releases.exs
@@ -2,22 +2,29 @@
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
-use Mix.Config
+import Config
 
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
+# database_url =
+#   System.get_env("DATABASE_URL") ||
+#     raise """
+#     environment variable DATABASE_URL is missing.
+#     For example: ecto://USER:PASS@HOST/DATABASE
+#     """
+
+db_host = System.get_env("DB_HOST")
+db_user = System.get_env("DB_USER")
+db_pw = System.get_env("DB_USER")
 
 config :hood_savy, HoodSavy.Repo,
-  # ssl: true,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  # url: database_url,
+  username: System.get_env("DB_USER"),
+  password: System.get_env("DB_PW"),
+  database: "hood_savy_prod",
+  hostname: System.get_env("DB_HOST"),
+  pool_size: String.to_integer(System.get_env("DB_POOL") || "10")
 
 secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
+  System.get_env("SECRET_KEY") ||
     raise """
     environment variable SECRET_KEY_BASE is missing.
     You can generate one by calling: mix phx.gen.secret
@@ -31,8 +38,8 @@ config :hood_savy, HoodSavyWeb.Endpoint,
 #
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
-#
-#     config :hood_savy, HoodSavyWeb.Endpoint, server: true
-#
+
+config :hood_savy, HoodSavyWeb.Endpoint, server: true
+
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
